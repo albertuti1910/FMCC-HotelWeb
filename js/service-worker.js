c51180b6542281c57c032c4f5d835a381a97075d
albertuti1910 <alberto.riveromonzon@gmail.com>
2024-10-16 16:42:33 +0100

    fix pwa

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,22 +1,49 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/style.css',
-        '/js/script.js',
-        '/images/logo.png'
-      ]);
-    })
-  );
+const CACHE_NAME = 'riu-palace-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/images/riu192x192.png',
+    '/images/riu512x512.png',
+    '/js/custom.js'
+    // Incluye aquí otros recursos que desees cachear
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+// Activación del Service Worker
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Intercepta peticiones y responde desde la caché si es posible
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
